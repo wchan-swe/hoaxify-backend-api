c83dd7f9752f901749cd466507ffc7824cff84de
Wayne Chan <wchan.it@outlook.com>
2022-05-27 10:02:11 -0500

    Add User entity validation

--- src/main/java/com/hoaxify/user/User.java
+++ src/main/java/com/hoaxify/user/User.java
@@ -1,21 +1,34 @@
 package com.hoaxify.user;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull
+    @Size(min = 4, max = 255)
     private String username;
+    @NotNull
+    @Size(min = 4, max = 255)
     private String displayName;
+    @NotNull
+    @Size(min = 8, max = 255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 
 }
